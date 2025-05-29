d2b1f5904db8c3ebd116450556344dedb7026496
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-05-29 16:14:56 +0200

    proj (setup): init commit
        - add
            - gitignore
            - readme
            - requirements
            - package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    author="Monde Anna",
+    author_email="monde.anna@enviro365.co.za",
+    name="stack kata",
+    description="TDD Kata using the stack data structure",
+    license="MIT",
+    version="1.0.0"
+)
