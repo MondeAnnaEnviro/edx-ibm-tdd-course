fbf2f47ac6b9fce17d07264b2ab6afd924b3816d
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-05-30 12:03:52 +0200

    stack (push): increases size by one

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,3 +4,7 @@ from stack.stack import Stack
 
 def test_new_stack_is_empty():
     assert Stack().empty()
+
+
+def test_pushing_once_increases_size_by_one():
+    assert Stack().push( 100 ).size() == 1
