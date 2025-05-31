f4b8d2c55a0f61f9cefa6cafb240070b27a752ca
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-05-31 08:35:43 +0200

    stack (isEmpty): be true when stack is empty

--- src/test/java/za/co/anna/monde/stack/StackTest.java
+++ src/test/java/za/co/anna/monde/stack/StackTest.java
@@ -1,9 +1,12 @@
 package za.co.anna.monde.stack;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class StackTest {
 
-
+    @Test
+    public void stackIsEmptyByDefault(){
+        assertTrue( new Stack().isEmpty() );
+    }
 }
