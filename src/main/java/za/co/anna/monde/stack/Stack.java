000ddb0557ea37d1e5d80eb14cbb3b1acb1724d5
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-05-31 08:09:40 +0200

    proj (setup): init commit
        - add
            - readme
            - gitignore
            - maven package

---
+++ src/main/java/za/co/anna/monde/stack/Stack.java
@@ -0,0 +1,7 @@
+package za.co.anna.monde.stack;
+
+import java.util.List;
+
+class Stack {
+
+}
---
+++ src/test/java/za/co/anna/monde/stack/StackTest.java
@@ -0,0 +1,9 @@
+package za.co.anna.monde.stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class StackTest {
+
+
+}
