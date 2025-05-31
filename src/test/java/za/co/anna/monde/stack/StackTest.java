9cdb326e7aa655bc3c319dad09e2230a70865634
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-05-31 09:46:11 +0200

    refactor (test): extract before each fixture

--- src/test/java/za/co/anna/monde/stack/StackTest.java
+++ src/test/java/za/co/anna/monde/stack/StackTest.java
@@ -4,40 +4,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StackTest {
 
+    private Stack stack;
+
+    @BeforeEach
+    public void setUp(){
+        stack = new Stack();
+    }
+
     @Test
     public void stackIsEmptyByDefault(){
-        assertTrue( new Stack().isEmpty() );
+        assertTrue( stack.isEmpty() );
     }
 
     @Test
     public void stackIsNotEmptyWhenItemAdded(){
-        assertFalse( new Stack().push( 0 ).isEmpty() );
+        assertFalse( stack.push( 0 ).isEmpty() );
     }
 
     @Test
     public void anEmptyStackHasSizeOfZero(){
-        assertEquals( 0, new Stack().size() );
+        assertEquals( 0, stack.size() );
     }
 
     @Test
     public void firstPushIncreasesSizeToOne(){
-        assertEquals( 1, new Stack().push( 10 ).size() );
+        assertEquals( 1, stack.push( 10 ).size() );
     }
 
     @Test
     public void afterNthPushStackIsSizeN(){
-        assertEquals( 2, new Stack().push( 1 ).push( 2 ).size() );
+        assertEquals( 2, stack.push( 1 ).push( 2 ).size() );
     }
 
     @Test
     public void peekingEmptyStackThrowsException(){
         Exception exception = assertThrows(
                 IllegalStateException.class,
-                () -> new Stack().peek()
+                () -> stack.peek()
         );
 
         assertEquals( "unable to peek at empty stack", exception.getMessage() );
@@ -45,14 +53,14 @@ class StackTest {
 
     @Test
     public void peekingAtPopulatedStackProvidesLastEntry(){
-        assertEquals( 33, new Stack().push( 3 ).push( 0 ).push( 33 ).peek() );
+        assertEquals( 33, stack.push( 3 ).push( 0 ).push( 33 ).peek() );
     }
 
     @Test
     public void poppingEmptyStackThrowsException(){
         Exception exception = assertThrows(
                 IllegalStateException.class,
-                () -> new Stack().pop()
+                () -> stack.pop()
         );
 
         assertEquals( "unable to pop from empty stack", exception.getMessage() );
@@ -60,7 +68,7 @@ class StackTest {
 
     @Test
     public void poppingAtPopulatedStackRemovesAndReturnsLastEntry(){
-        Stack stack = new Stack().push( 52 ).push( 184 ).push( -2 );
+        stack.push( 52 ).push( 184 ).push( -2 );
         assertEquals( -2, stack.pop() );
         assertEquals( 184, stack.pop() );
         assertEquals( 52, stack.pop() );
@@ -68,8 +76,7 @@ class StackTest {
 
     @Test
     public void multiOperationalProcessing(){
-        Stack stack = new Stack().push( 23 );
-        assertEquals( 1, stack.size() );
+        assertEquals(  1, stack.push( 23 ).size() );
         assertEquals( 33, stack.push( 15 ).push( 33 ).peek() );
         assertEquals( 33, stack.pop() );
     }
