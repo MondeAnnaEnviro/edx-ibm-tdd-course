907f88f1ce1403cdae90b300558848f2570f3373
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-01 23:12:20 +0200

    tests (peek): where many items present, left-most item shows

--- tests/StackTest.hs
+++ tests/StackTest.hs
@@ -9,3 +9,9 @@ main = hspec $ do
   describe "peek" $ do
     it "where one item present, said item shows" $ do
       peek ( Stack [ 44 ]) `shouldBe` ( 44 :: Int )
+
+    it "where many items present, left-most item shows" $ do
+      {- recall: adding uses `cons`, thus prepends to list
+       - consequently, the left most item is the last addition
+       -}
+      peek ( Stack [ 653, 1, 66, 903, 43 ]) `shouldBe` ( 653 :: Int )
