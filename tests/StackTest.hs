17d5b7e7b5fcb2960212743cfbac7de140ee6173
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-01 23:15:58 +0200

    tests (peek): works with all types, while being type specific

--- tests/StackTest.hs
+++ tests/StackTest.hs
@@ -15,3 +15,9 @@ main = hspec $ do
        - consequently, the left most item is the last addition
        -}
       peek ( Stack [ 653, 1, 66, 903, 43 ]) `shouldBe` ( 653 :: Int )
+
+    it "works with all types, while being type specific" $ do
+      peek ( Stack [ 's', 't', 'a', 'r' ]) `shouldBe` ( 's' :: Char )
+      peek ( Stack [ "star" ]) `shouldBe` ( "star" :: String )
+      peek ( Stack [ 44.44 ]) `shouldBe` ( 44.44 :: Double )
+      peek ( Stack [ 0.11 ]) `shouldBe` ( 0.11 :: Float )
