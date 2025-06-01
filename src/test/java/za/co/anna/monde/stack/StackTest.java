83c040ce5dce731fa53c8161a89aa570a0ed4fb8
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-01 08:41:50 +0200

    stack (isEmpty): false when more that zero items are part of stack

--- src/test/java/za/co/anna/monde/stack/StackTest.java
+++ src/test/java/za/co/anna/monde/stack/StackTest.java
@@ -1,5 +1,6 @@
 package za.co.anna.monde.stack;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,10 @@ class StackTest {
     @Test
     public void stackIsEmptyByDefault(){
         assertTrue( new Stack().isEmpty() );
+    }
+
+    @Test
+    public void stackIsNotEmptyWhenItemAdded(){
+        assertFalse( new Stack().push( 0 ).isEmpty() );
     }
 }
