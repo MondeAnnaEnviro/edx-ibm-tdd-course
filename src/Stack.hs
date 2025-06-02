18e6c7428326f61561fb9f03a7e40914beabb971
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-02 03:36:22 +0200

    stack (empty): instanciate `empty` stack as instance

--- src/Stack.hs
+++ src/Stack.hs
@@ -4,6 +4,10 @@ module Stack where
 data Stack a = Stack [ a ] deriving ( Show, Eq )
 
 
+empty :: Stack a
+empty = Stack []
+
+
 isEmpty :: Stack a -> Bool
 isEmpty ( Stack x ) = null x
 
@@ -12,9 +16,9 @@ peek :: Stack a -> a
 peek ( Stack x ) = head x
 
 
-size :: Stack a -> Int
-size ( Stack x ) = length x
-
-
 push :: a -> Stack a -> Stack a
 push x ( Stack xs ) = Stack ( x:xs )
+
+
+size :: Stack a -> Int
+size ( Stack x ) = length x
