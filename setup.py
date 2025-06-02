e87f588eebdda45f7837887fcfbef2abc24dda37
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-02 11:17:38 +0200

    proj (setup): init commit
        - add
            - gitignore
            - readme
            - requirements
            - package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    author="Monde Anna",
+    author_email="monde.anna@enviro365.co.za",
+    name="queue kata",
+    description="TDD Kata using the queue data structure",
+    license="MIT",
+    version="1.0.0"
+)
