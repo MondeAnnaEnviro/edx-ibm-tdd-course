2b38f097eb3613ca5924896af6fad97d64ee191b
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-02 05:04:25 +0200

    tests (refactor): use the stack `empty` where appropriate

--- tests/StackTest.hs
+++ tests/StackTest.hs
@@ -1,10 +1,13 @@
 module Main where
 
+
 import Stack
 import Test.Hspec
 
+
 main :: IO ()
 main = hspec $ do
+
 
   describe "peek" $ do
     it "where one item present, said item shows" $ do
@@ -22,28 +25,31 @@ main = hspec $ do
       peek ( Stack [ 44.44 ]) `shouldBe` ( 44.44 :: Double )
       peek ( Stack [ 0.11 ]) `shouldBe` ( 0.11 :: Float )
 
+
   describe "isEmpty" $ do
     it "where no items present, return true" $ do
-      isEmpty ( Stack [] ) `shouldBe` True
+      isEmpty empty `shouldBe` True
 
     it "where items present, return false" $ do
       isEmpty ( Stack [ "value" ]) `shouldBe` False
 
+
   describe "size" $ do
     it "where no items, returns zero" $ do
-      size ( Stack [] ) `shouldBe` ( 0 :: Int )
+      size empty `shouldBe` ( 0 :: Int )
 
     it "where items present, returns number of items present" $ do
       size (( Stack [ 5, 5, 5, 5 ]) :: Stack Int ) `shouldBe` ( 4 :: Int )
 
+
   describe "push" $ do
     it "where no items present, should add one item" $ do
-      let stack = push "word" ( Stack [] )
+      let stack = push "word" empty
 
       isEmpty stack `shouldBe` False
       peek stack `shouldBe` ( "word" :: String )
       size stack `shouldBe` ( 1 :: Int )
 
     it "n pushes makes for n sized stack" $ do
-      let stack = push 4 $ push 77 $ push 4 ( Stack [] )
+      let stack = push 4 $ push 77 $ push 4 empty
       size ( stack :: Stack Int ) `shouldBe` ( 3 :: Int )
