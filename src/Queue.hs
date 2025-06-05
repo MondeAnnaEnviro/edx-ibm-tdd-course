237ee020dd8a3d616b1673564193292d8f16fca4
MondeAnna <monde.anna@gmail.com>
2025-06-05 13:10:46 +0200

    queue (refactor): alter placement of parentheses

--- src/Queue.hs
+++ src/Queue.hs
@@ -24,16 +24,16 @@ dequeue :: Queue a -> ( Maybe a, Queue a )
 dequeue ( Queue [] ) = ( Nothing, Queue [] )
 dequeue ( Queue ( x:xs ))
  | isEmpty ( Queue xs ) = ( Just x, Queue [] )
- | otherwise = ( peek ( Queue xs ), Queue ( x : init xs ))
+ | otherwise = ( peek $ Queue xs, Queue $ x : init xs )
 
 
 enqueue :: a -> Queue a -> Queue a
 enqueue x ( Queue [] ) = Queue [ x ]
-enqueue x ( Queue xs ) = Queue ( x:xs )
+enqueue x ( Queue xs ) = Queue $ x:xs
 
 
 size :: Queue a -> Int
 size ( Queue [] ) = 0
 size ( Queue ( x:xs ))
  | isEmpty ( Queue xs ) = 1
- | otherwise = 1 + size ( Queue xs )
+ | otherwise = 1 + ( size $ Queue xs )
