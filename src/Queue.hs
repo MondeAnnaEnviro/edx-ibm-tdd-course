0479af010d370976843d205abb13bf8f72725925
MondeAnna <monde.anna@gmail.com>
2025-06-05 10:14:29 +0200

    queue (size): peek into empty queue shows nothing

--- src/Queue.hs
+++ src/Queue.hs
@@ -9,6 +9,12 @@ isEmpty ( Queue [] ) = True
 isEmpty ( Queue _ ) = False
 
 
+peek :: Queue a -> Maybe a
+peek ( Queue [] ) = Nothing
+
+
 size :: Queue a -> Int
 size ( Queue [] ) = 0
-size ( Queue x ) = 1
+size ( Queue ( x:xs ))
+ | isEmpty ( Queue xs ) = 1
+ | otherwise = 1 + size ( Queue xs )
--- tests/QueueTest.hs
+++ tests/QueueTest.hs
@@ -17,9 +17,17 @@ main = hspec $ do
       isEmpty ( Queue [ 1 ] ) `shouldBe` False
 
 
+  describe "\n\npeek" $ do
+    it "queue with no entries returns nothing" $ do
+      peek ( Queue [] ) `shouldBe` ( Nothing :: Maybe Char )
+
+
   describe "\n\nsize" $ do
     it "queue with no entries is size zero" $ do
       size ( Queue [] ) `shouldBe` ( 0 :: Int )
 
     it "queue with one entry is size one" $ do
       size ( Queue [ 3 ]) `shouldBe` ( 1 :: Int )
+
+    it "queue with n entrie is size n" $ do
+      size ( Queue [ 0, 1, 2, 3 ]) `shouldBe` ( 4 :: Int )
