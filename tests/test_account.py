ca0ad5ae3103e077787fa84f4fcd8d6381d22082
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-12 18:23:02 +0200

    class method (all): querying all when sessin has data returns all data

--- tests/test_account.py
+++ tests/test_account.py
@@ -13,6 +13,16 @@ from models import app
 
 
 @pytest.fixture( scope="session" )
+def account_data():
+    """Loads JSON data from a file."""
+    fixture_path = "/tests/fixtures/account_data.json"
+    file_path = str( Path.cwd() ) + fixture_path
+
+    with Path( file_path ).open() as file:
+        return json.load( file )
+
+
+@pytest.fixture( scope="session" )
 def app_context():
     with app.app_context():
         yield
@@ -25,3 +35,12 @@ def test_querying_all_when_session_is_empty_returns_empty_list( app_context ):
 
         mock_query.all.assert_called_once()
         assert result == []
+
+
+def test_querying_all_when_session_has_data_returns_all_data( app_context, account_data ):
+    with patch( "models.account.Account.query" ) as mock_query:
+        mock_query.all.return_value = account_data
+        result = Account.all()
+
+        mock_query.all.assert_called_once()
+        assert result == account_data
