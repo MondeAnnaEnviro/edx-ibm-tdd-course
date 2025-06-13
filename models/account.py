dcfcff1ac1c31c8bb89beb1f2503e7d7a7b2fe8e
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-13 18:22:04 +0200

    class method (refactor): `find` -> use mock session when making call with valid `id`

--- models/account.py
+++ models/account.py
@@ -24,7 +24,6 @@ class Account( db.Model ):
     @classmethod
     def all( cls ) -> list:
         """Returns all of the Accounts in the database"""
-
         logger.info("Processing all Accounts")
         return cls.query.all()
 
--- tests/test_account.py
+++ tests/test_account.py
@@ -2,16 +2,33 @@
 Test Cases TestAccountModel
 """
 
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 from unittest.mock import patch
-from datetime import datetime
+from unittest import mock
 from pathlib import Path
-import random
+import datetime
 import pytest
 import json
 
 
 from models.account import Account
 from models import app
+
+
+@pytest.fixture( scope="session" )
+def mock_session( data_in_account_format ):
+    data = [
+        ([ mock.call.query( Account ), mock.call.filter( Account.id == _id )],[ account ])
+        for _id, account
+        in enumerate( data_in_account_format, start=1 )
+    ]
+
+    _mock_session = UnifiedAlchemyMagicMock( data=data )
+
+    yield _mock_session
+
+    _mock_session.rollback()
+    _mock_session.close()
 
 
 @pytest.fixture( scope="session" )
@@ -27,7 +44,7 @@ def data_in_dict_format():
 @pytest.fixture( scope="session" )
 def data_in_account_format( data_in_dict_format ):
     return [
-        Account( id=_id, **account, date_joined=datetime.now() )\
+        Account( id=_id, **account, date_joined=datetime.datetime.now() )
         for _id, account
         in enumerate( data_in_dict_format, start=1 )
     ]
@@ -69,17 +86,14 @@ def test_calling_find_using_invalid_id( app_context ):
         mock_query.return_value.filter.return_value.first.assert_called_once()
 
 
-def test_calling_find_with_valid_id( app_context, data_in_account_format ):
-    """I don't like this test"""
-
-    account = random.choice( data_in_account_format )
-    target = "models.account.Account.query.session.query"
+def test_calling_find_using_valid_id( app_context, mock_session, data_in_account_format ):
+    target = "models.account.Account.query"
 
     with patch( target ) as mock_query:
-        mock_query.return_value.filter.return_value.first.return_value = account
+        mock_query.session = mock_session
 
-        result = Account.find( 111 )
+        for _id, account in enumerate( data_in_account_format, start=1 ):
+            result = Account.find( _id )
 
-        assert result is account
-
-        mock_query.return_value.filter.return_value.first.assert_called_once()
+            assert result == account
+            mock_session.query.return_value.filter.return_value.first.assert_called()
