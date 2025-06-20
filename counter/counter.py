5ae11c1d563b92ba9dfc9206c3b607aa6e907569
MondeAnnaEnviro <monde.anna@enviro365.co.za>
2025-06-20 07:14:20 +0200

    counter (put): n updates increases count by n
    # Conflicts:
    #	counter/counter.py

--- counter/counter.py
+++ counter/counter.py
@@ -2,26 +2,34 @@ from flask import Flask, Response
 from counter import status
 
 
-app = Flask( __name__ )
-COUNTERS = {}
+def create_counter_container() -> dict:
+    return {}
 
 
-@app.route( "/counters/<name>", methods=[ "POST" ])
-def create_counter( name, counters=COUNTERS ):
-    """Creates a counter"""
-    app.logger.info( f"Request to create counter: {name}" )
+def create_app() -> Flask:
+    app = Flask( __name__, instance_relative_config=True )
+    counters = create_counter_container()
 
-    if name in counters:
-        return { "message": f"Counter {name} already exists" }, status.HTTP_409_CONFLICT
+    @app.route( "/counters/<name>", methods=[ "POST" ])
+    def create_counter( name: str ) -> Response:
+        """Creates a counter"""
+        app.logger.info( f"Request to create counter: {name}" )
 
-    counters[ name ] = 0
-    return { name: counters[ name ]}, status.HTTP_201_CREATED
+        if name in counters:
+            return (
+                { "message": f"Counter {name} already exists" },
+                status.HTTP_409_CONFLICT,
+            )
 
+        counters[ name ] = 0
+        return ({ name: counters[ name ]}, status.HTTP_201_CREATED )
 
-@app.route( "/counters/<name>", methods=[ "PUT" ])
-def update_counter( name: str, counters: dict = COUNTERS ) -> Response:
-    """Updates a counter"""
-    app.logger.info( f"Request to update counter: {name}" )
+    @app.route( "/counters/<name>", methods=[ "PUT" ])
+    def update_counter( name: str ) -> Response:
+        """Updates a counter"""
+        app.logger.info( f"Request to update counter: {name}" )
 
-    counters[ name ] += 1
-    return { name: counters[ name ]}, status.HTTP_200_OK
+        counters[ name ] += 1
+        return ({ name: counters[ name ]}, status.HTTP_200_OK )
+
+    return app
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -8,9 +8,9 @@ from unittest import TestCase
 from counter import counter, status
 
 
-@pytest.fixture
+@pytest.fixture( scope="function" )
 def client():
-    return counter.app.test_client()
+    yield counter.create_app().test_client()
 
 
 def test_creating_new_counter( client ):
@@ -24,8 +24,8 @@ def test_creating_new_counter( client ):
 
 def test_creating_duplicate_counter( client ):
     """It should return an error for duplicates"""
-    first_response = client.post( "/counters/item2" )
-    second_response = client.post( "/counters/item2" )
+    first_response = client.post( "/counters/item" )
+    second_response = client.post( "/counters/item" )
 
     assert first_response.status_code == status.HTTP_201_CREATED
     assert second_response.status_code == status.HTTP_409_CONFLICT
@@ -39,3 +39,18 @@ def test_updating_a_counter( client ):
 
     assert response.status_code == status.HTTP_200_OK
     assert data[ "item" ] == 1
+
+
+def test_updating_counter_multiple_times( client ):
+    """It should update a counter"""
+    client.post( "/counters/item" )
+
+    num_calls = 10
+
+    for _ in range( num_calls ):
+        response = client.put( "/counters/item" )
+
+    data = response.get_json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert data[ "item" ] == num_calls
